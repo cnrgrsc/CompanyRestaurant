b3e78bfbae56bd69f478d1f630f5fd8f5a380306
Sselentt <sselentt23@gmail.com>
2024-03-26 23:28:41 +0300

    güncel

--- CompanyRestaurant.MVC/Areas/Admin/Controllers/TableController.cs
+++ CompanyRestaurant.MVC/Areas/Admin/Controllers/TableController.cs
@@ -1,10 +1,16 @@
 ﻿using AutoMapper;
 using CompanyRestaurant.BLL.Abstracts;
+using CompanyRestaurant.BLL.Services;
+using CompanyRestaurant.Entities.Entities;
+using CompanyRestaurant.MVC.Models.SupplierVM;
+using CompanyRestaurant.MVC.Models.TableVM;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyRestaurant.MVC.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class TableController : Controller
     {
         private readonly ITableRepository _tableRepository;
@@ -18,13 +24,88 @@ namespace CompanyRestaurant.MVC.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var table = await _tableRepository.GetAllAsync();
-            return View(table);
+            var tables = await _tableRepository.GetAllAsync();
+            var model = _mapper.Map<IEnumerable<TableViewModel>>(tables);
+            return View(model);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TableViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var table = _mapper.Map<Table>(model);
+                await _tableRepository.CreateAsync(table);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var table = await _tableRepository.GetByIdAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<TableViewModel>(table);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(TableViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var table = _mapper.Map<Table>(model);
+                await _tableRepository.UpdateAsync(table);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var table = await _tableRepository.GetByIdAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<TableViewModel>(table);
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var models = await _tableRepository.GetByIdAsync(id);
+            if (models == null)
+            {
+                return NotFound();
+            }
+            await _tableRepository.DestroyAsync(models);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var table = await _tableRepository.GetByIdAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<TableViewModel>(table);
+            return View(model);
         }
 
     }
--- CompanyRestaurant.MVC/Areas/Admin/Controllers/UnitStockController.cs
+++ CompanyRestaurant.MVC/Areas/Admin/Controllers/UnitStockController.cs
@@ -1,11 +1,116 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CompanyRestaurant.BLL.Abstracts;
+using CompanyRestaurant.BLL.Services;
+using CompanyRestaurant.Entities.Entities;
+using CompanyRestaurant.MVC.Models.CategoryVM;
+using CompanyRestaurant.MVC.Models.UnitStockVM;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyRestaurant.MVC.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class UnitStockController : Controller
     {
-        
+        private readonly IUnitStockRepository _unitStockRepository;
+        private readonly IMapper _mapper;
+
+        public UnitStockController(IUnitStockRepository unitStockRepository, IMapper mapper)
+        {
+            _unitStockRepository = unitStockRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var unitStocks = await _unitStockRepository.GetAllAsync();
+            var model = _mapper.Map<IEnumerable<UnitStockViewModel>>(unitStocks);
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UnitStockViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var unitStock = _mapper.Map<UnitStock>(model);
+                await _unitStockRepository.CreateAsync(unitStock);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var unitStock = await _unitStockRepository.GetByIdAsync(id);
+            if (unitStock == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<UnitStockViewModel>(unitStock);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UnitStockViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var unitStock = _mapper.Map<UnitStock>(model);
+                await _unitStockRepository.UpdateAsync(unitStock);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var unitStock = await _unitStockRepository.GetByIdAsync(id);
+            if (unitStock == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<UnitStockViewModel>(unitStock);
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var unitStock = await _unitStockRepository.GetByIdAsync(id);
+            if (unitStock == null)
+            {
+                return NotFound();
+            }
+
+            await _unitStockRepository.DestroyAsync(unitStock);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var unitStock = await _unitStockRepository.GetByIdAsync(id);
+            if (unitStock == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<UnitStockViewModel>(unitStock);
+            return View(model);
+        }
+
+
+
+
+
 
     }
 }
